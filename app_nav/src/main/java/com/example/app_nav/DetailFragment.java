0767f278fe51153b123ce40ca33dcabdd893d48e
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 11:30:11 +0800

    android

--- app_nav/src/main/java/com/example/app_nav/DetailFragment.java
+++ app_nav/src/main/java/com/example/app_nav/DetailFragment.java
@@ -2,11 +2,16 @@ package com.example.app_nav;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class DetailFragment extends Fragment {
 
@@ -15,5 +20,15 @@ public class DetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn = view.findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+            NavController controller = Navigation.findNavController(v);
+            controller.navigate(R.id.action_detailFragment_to_homeFragment);
+        });
     }
 }
