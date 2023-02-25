a16881972e5b2503ef63649196544a601cc74e3b
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 12:31:07 +0800

    android

--- app_nav/src/main/java/com/example/app_nav/DetailFragment.java
+++ app_nav/src/main/java/com/example/app_nav/DetailFragment.java
@@ -12,8 +12,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class DetailFragment extends Fragment {
+
+    private Bundle bundle;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 接收參數
+        bundle = getArguments();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +35,12 @@ public class DetailFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button btn = view.findViewById(R.id.button);
+        TextView tv = view.findViewById(R.id.tv);
+        String msg = tv.getText().toString() + "\n" +
+                bundle.getString("remote_name") + "\n" +
+                bundle.getInt("remote_age") + "\n" +
+                bundle.getSerializable("user");
+        tv.setText(msg);
         btn.setOnClickListener(v -> {
             NavController controller = Navigation.findNavController(v);
             controller.navigate(R.id.action_detailFragment_to_infoFragment);
--- app_nav/src/main/java/com/example/app_nav/model/User.java
+++ app_nav/src/main/java/com/example/app_nav/model/User.java
@@ -30,4 +30,12 @@ public class User implements Serializable {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
