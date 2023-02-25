f42235589b410b8a11316849776011bbac01473a
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 11:29:19 +0800

    android

--- app_nav/src/main/java/com/example/app_nav/HomeFragment.java
+++ app_nav/src/main/java/com/example/app_nav/HomeFragment.java
@@ -2,11 +2,16 @@ package com.example.app_nav;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 // Fragment 生命週期
 // https://img-blog.csdnimg.cn/img_convert/8998b924006e95802ef00a14f98870aa.png
@@ -16,5 +21,15 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn = view.findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+            NavController controller = Navigation.findNavController(v);
+            controller.navigate(R.id.action_homeFragment_to_detailFragment);
+        });
     }
 }
