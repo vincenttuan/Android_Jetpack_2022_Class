e3aa3d06f76d58ee3230a7201639a13be2b4ca19
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 14:47:50 +0800

    android

--- app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/MainActivity.java
+++ app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.app_stock_fastapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NavController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 因為 activity_main 裡面放的是 NavHostFragment
+        // 所以要先得到 NavHostFragment 再取 NavController
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        controller = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, controller);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return controller.navigateUp();
     }
 }
