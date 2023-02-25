3562359c9fbad037e1137bedfb8360cf5a1cdde6
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 16:41:01 +0800

    android

--- app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/SymbolListFragment.java
+++ app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/SymbolListFragment.java
@@ -2,18 +2,41 @@ package com.example.app_stock_fastapi;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.app_stock_fastapi.service.StockService;
 
 public class SymbolListFragment extends Fragment {
+    private StockService stockService;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        stockService = new StockService();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_symbol_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView info = view.findViewById(R.id.info);
+
+        stockService.queryAll(list -> {
+            info.setText(list.size() + "筆" + "\n" + list);
+        });
+
     }
 }
