34c4df508ca7f3b6fbee3cd43bb35c1d9840f946
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 14:50:17 +0800

    android

--- app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/WelcomeFragment.java
+++ app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/WelcomeFragment.java
@@ -2,7 +2,11 @@ package com.example.app_stock_fastapi;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,5 +19,15 @@ public class WelcomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.button).setOnClickListener(v -> {
+            NavController controller = Navigation.findNavController(v);
+            Bundle bundle = new Bundle();
+            controller.navigate(R.id.action_welcomeFragment_to_mainFragment, bundle);
+        });
     }
 }
