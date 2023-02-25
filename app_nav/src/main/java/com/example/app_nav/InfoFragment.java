394140413881afb1f90a456f28dea040e3984564
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 12:12:27 +0800

    android

--- app_nav/src/main/java/com/example/app_nav/InfoFragment.java
+++ app_nav/src/main/java/com/example/app_nav/InfoFragment.java
@@ -1,6 +1,10 @@
 package com.example.app_nav;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,18 +12,13 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-public class DetailFragment extends Fragment {
+public class InfoFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        return inflater.inflate(R.layout.fragment_info, container, false);
     }
 
     @Override
@@ -28,7 +27,7 @@ public class DetailFragment extends Fragment {
         Button btn = view.findViewById(R.id.button);
         btn.setOnClickListener(v -> {
             NavController controller = Navigation.findNavController(v);
-            controller.navigate(R.id.action_detailFragment_to_infoFragment);
+            controller.navigate(R.id.action_infoFragment_to_homeFragment);
         });
     }
 }
