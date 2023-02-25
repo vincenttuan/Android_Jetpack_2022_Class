2451d2ee8a83bc4e715f8cceeed04fa5757a797a
MB-teacher <vincentjava@yahoo.com.tw>
2023-02-25 14:51:47 +0800

    android

--- app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/MainFragment.java
+++ app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/MainFragment.java
@@ -2,7 +2,11 @@ package com.example.app_stock_fastapi;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,5 +19,22 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.searchButton).setOnClickListener(v -> {
+            NavController controller = Navigation.findNavController(v);
+            Bundle bundle = new Bundle();
+            controller.navigate(R.id.action_mainFragment_to_symbolFragment, bundle);
+        });
+
+        view.findViewById(R.id.queryButton).setOnClickListener(v -> {
+            NavController controller = Navigation.findNavController(v);
+            Bundle bundle = new Bundle();
+            controller.navigate(R.id.action_mainFragment_to_symbolListFragment, bundle);
+        });
     }
 }
