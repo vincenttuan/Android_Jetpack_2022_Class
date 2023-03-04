0bb2b26b6290c1d9a3084108fe4a363acd315156
MB-teacher <vincentjava@yahoo.com.tw>
2023-03-04 14:25:02 +0800

    android

--- app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/api/RetrofitClient.java
+++ app_stock_fastapi/src/main/java/com/example/app_stock_fastapi/api/RetrofitClient.java
@@ -3,6 +3,9 @@ package com.example.app_stock_fastapi.api;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,12 +15,18 @@ public class RetrofitClient {
     private Retrofit retrofit;
 
     private RetrofitClient() {
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .build();
+
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd HH:mm:ss") // 字串轉 Date (2023-02-25 07:38:39)
                 .create();
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(okHttpClient)
                 .build();
     }
 
