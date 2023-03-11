de14b5275a0697aabe225bb8a3034452d42ecad4
MB-teacher <vincentjava@yahoo.com.tw>
2023-03-11 12:06:52 +0800

    android

--- app_stock_ui_fastapi/src/main/java/com/example/app_stock_ui_fastapi/api/RetrofitClient.java
+++ app_stock_ui_fastapi/src/main/java/com/example/app_stock_ui_fastapi/api/RetrofitClient.java
@@ -3,21 +3,46 @@ package com.example.app_stock_ui_fastapi.api;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://10.0.2.2:8000";
+    //private static final String BASE_URL = "http://10.0.2.2:8000";
+    private static final String BASE_URL = "https://54.199.175.248";
+    //private static final String BASE_URL = "http://54.199.175.248:8000";
     private static RetrofitClient retrofitClient;
     private Retrofit retrofit;
 
-    private RetrofitClient() {
+    private RetrofitClient() throws Exception {
+
+        // 若 https 不是安全的, 可以使用一個不安全的 TrustManager，該 TrustManager 將信任所有證書，從而允許您訪問不安全的 HTTPS 網站
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+
         final OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .readTimeout(60, TimeUnit.SECONDS)
                 .connectTimeout(60, TimeUnit.SECONDS)
+                .hostnameVerifier((hostname, session) -> true)
+                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
                 .build();
 
         Gson gson = new GsonBuilder()
@@ -32,7 +57,11 @@ public class RetrofitClient {
 
     public static synchronized RetrofitClient getInstance() {
         if(retrofitClient == null) {
-            retrofitClient = new RetrofitClient();
+            try {
+                retrofitClient = new RetrofitClient();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
         return retrofitClient;
     }
