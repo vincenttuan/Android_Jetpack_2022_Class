684987f6b26fae7a61c120cb661c147d9e817740
MB-teacher <vincentjava@yahoo.com.tw>
2023-03-11 12:26:26 +0800

    android

--- app_stock_ui_fastapi/src/main/java/com/example/app_stock_ui_fastapi/api/RetrofitClient.java
+++ app_stock_ui_fastapi/src/main/java/com/example/app_stock_ui_fastapi/api/RetrofitClient.java
@@ -26,23 +26,29 @@ public class RetrofitClient {
 
         // 若 https 不是安全的, 可以使用一個不安全的 TrustManager，該 TrustManager 將信任所有證書，從而允許您訪問不安全的 HTTPS 網站
         TrustManager[] trustAllCerts = new TrustManager[] {
+                // 這個 X509TrustManager 實例的目的是覆蓋默認的安全檢查，使得這個 OkHttpClient 可以信任所有的 HTTPS 服務器證書，從而建立一個不安全的 HTTPS 連接。
                 new X509TrustManager() {
-                    public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-                    public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        // 空實作
+                    }
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        // 空實作
+                    }
                     public X509Certificate[] getAcceptedIssuers() {
                         return new X509Certificate[0];
                     }
                 }
         };
 
+        // 建立一個 SSLContext 環境
         SSLContext sslContext = SSLContext.getInstance("SSL");
         sslContext.init(null, trustAllCerts, new SecureRandom());
 
         final OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .readTimeout(60, TimeUnit.SECONDS)
                 .connectTimeout(60, TimeUnit.SECONDS)
-                .hostnameVerifier((hostname, session) -> true)
-                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
+                .hostnameVerifier((hostname, session) -> true) // 讓主機名稱永遠通過
+                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0]) // 配置 SSL Socket
                 .build();
 
         Gson gson = new GsonBuilder()
